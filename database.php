55eeeccb0cddf9f7818a76efbf796b1ed48580e4
Gilang291140 <154669975+Gilang291140@users.noreply.github.com>
2026-05-17 01:51:03 +0800

    Add files via upload

---
+++ database.php
@@ -0,0 +1,31 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json; charset=UTF-8");
+
+class Database {
+    private $host = "localhost";
+    private $db_name = "halal_food_db";  // Nama database Anda
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8", 
+                $this->username, 
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $this->conn->exec("set names utf8");
+        } catch(PDOException $exception) {
+            error_log("Connection error: " . $exception->getMessage());
+        }
+        return $this->conn;
+    }
+}
+?>
